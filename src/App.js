44c023fa1d19a4e0273cc78ab0806846268a1fae
andreitecsor <andrei.tecsor@gmail.com>
2021-04-12 14:21:06 +0300

    added node-sass, react-router-dom; organising project structure; added Economica font.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import {Route, Switch} from 'react-router-dom';
+import HomePage from "./pages/homepage/homepage.component";
+import Header from "./components/header/header.component";
+
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Collectio. The act of collecting. Together.
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        This is my homepage
+    </div>
+
+)
+
+export default HomePage;
